7a55b4c2e225452c55bdf8e0b7a6b19063663274
bg012030 <bg012030@yahoo.com.tw>
2014-10-20 22:06:36 +0800

    new

--- 2.py
+++ 2.py
@@ -1,9 +1,7 @@
-score1 = int(input ("請輸入第一個成績\n"))
-score2 = int(input ("請輸入第二個成績\n"))
-score3 = int(input ("請輸入第三個成績\n"))
-list1 = [score1,score2,score3]
-list1.sort()
-list1[0] = int(list1[0]) * 0.2
-list1[1] = int(list1[1]) * 0.3
-list1[2] = int(list1[2]) * 0.5
-print ("期末成績為" , list1[0]+list1[1]+list1[2])
+score = [] ; score2 = [0.2,0.3,0.5]
+for w in range(1,4):
+  score.append(int(input("請輸入第%s個成績\n" % (w))))
+score.sort()
+for w in range(0,3):
+  score[w] = int(score[w]) * score2[w]
+print ("期末成績為", score[0]+score[1]+score[2])
