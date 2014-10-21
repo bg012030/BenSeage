6bf31c75c7361557da6001e19b6619455e3c9ee0
bg012030 <bg012030@yahoo.com.tw>
2014-10-21 14:38:31 +0800

    new(4)

--- 3.py
+++ 3.py
@@ -3,11 +3,11 @@ n = int(input("請輸入參加人數\n"))
 list1 = []
 list2 = []
 for w in range(0,n):
-  a = input("請輸入'稱號 數字'\n")
+  a = input("請輸入第%s位的「稱號 數字」\n" % (w+1))
   list1.extend( a.split(" "))
   list2.append( abs(int(list1[w*2+1]) - s) )
 
-print ("=================")
+print ("==========================")
 for w in range(0,n):
   if int(list2[w]) == sorted(list2)[n-1]:
-    print (list1[w*2] , "要喝金盃拉茶") 
+    print (list1[w*2] , "要喝金盃拉茶")
